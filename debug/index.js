5e6c68ffc9abd23bfa0b9c9c5e72592af8b58d5e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-08-18 13:04:27 +0200

    small fixes

--- debug/index.js
+++ debug/index.js
@@ -14,40 +14,47 @@ const svg = d3.select('#viz');
 
 const xAxis = svg.append('g');
 const yAxis = svg.append('g');
-const paths = svg.append('g');
+const lines = svg.append('g');
 
 const colors = d3.scaleOrdinal(d3.schemeCategory10);
 
 function viz() {
+  // check length
   if (list.length === 0) {
     return;
   }
 
-  const x = d3
+  // prepare x scale
+  const xScale = d3
     .scaleTime()
     .domain(d3.extent(list, (d) => d.time))
     .range([0, width]);
 
-  xAxis.attr('transform', 'translate(50,' + (50 + height) + ')').call(d3.axisBottom(x));
+  // apply x scale
+  xAxis.attr('transform', 'translate(50,' + (50 + height) + ')').call(d3.axisBottom(xScale));
 
-  const y = d3.scaleLinear().domain([0, 1]).range([height, 0]);
+  // prepare y scale
+  const yScale = d3.scaleLinear().domain([0, 1]).range([height, 0]);
 
-  yAxis.attr('transform', 'translate(50,50)').call(d3.axisLeft(y));
+  // apply y scale
+  yAxis.attr('transform', 'translate(50,50)').call(d3.axisLeft(yScale));
 
+  // prepare lin generator
   const line = (i) => {
     return d3
       .line()
       .x(function (d) {
-        return 50 + x(d.time);
+        return 50 + xScale(d.time);
       })
       .y(function (d) {
-        return 50 + y(d.sample[i]);
+        return 50 + yScale(d.sample[i]);
       });
   };
 
-  paths
+  // update lines
+  lines
     .selectAll('path')
-    .data(new Array(list[0].sample.length))
+    .data(new Array(d3.max(list, d => d.sample.length)))
     .join('path')
     .attr('fill', 'none')
     .attr('stroke', (_, i) => colors(i))
